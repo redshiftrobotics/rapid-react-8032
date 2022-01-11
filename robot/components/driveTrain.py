2445cc161ee2e4d997e5df2166fd8b187c6fd2fc
AchillesHam <achillesham@seattleacademy.org>
2022-01-10 16:41:54 -0800

    Add Arcade Drive Function

--- robot/components/driveTrain.py
+++ robot/components/driveTrain.py
@@ -3,3 +3,7 @@ import wpilib
 import rev
 
 class DriveTrain:
+    def arcadeDrive(self, xAxis,yAxis):
+        self.maxSpeed = 0.2
+        self.rightMotorSpeed = yAxis-xAxis
+        self.leftMotorSpeed= yAxis+xAxis
