ec8a8bb1b7d9e035f37c6e6e8728cbe52f56554c
Jenna-LI <58526103+Jenna-LI@users.noreply.github.com>
2022-01-15 11:59:01 -0800

    added joystick. Tank drive not operational

--- robot/robot.py
+++ robot/robot.py
@@ -9,8 +9,6 @@ class MyRobot(magicbot.MagicRobot):
     
     def createObjects(self):
         self.driverJoystick = wpilib.Joystick(0)
-        #we do not know why it has to be setZChannel. We also don't underdstand the 5. 
-        self.driverJoystick.setZChannel(5)
 
         self.frontLeftMotor = rev.CANSarkMax(0, rev.CANSparkMaxLowLevel.kBrushless)
         self.frontRightMotor = rev.CANSarkMax(1, rev.CANSparkMaxLowLevel.kBrushless)
@@ -24,10 +22,24 @@ class MyRobot(magicbot.MagicRobot):
         #get joystick data
         #call tank drive
         self.driveTrain.enabled()
-        leftJoystick = self.driverJoystick.getY()
-        rightJoystick = self.driverJoystick.getZ()
 
-        self.driveTrain.tank_drive(self.speed*leftJoystick, self.speed*rightJoystick)
+        #logitech extreme 3d. Case 10
+
+        #get y axis - 1 (when moving joystick forward and backwards)
+        wpilib.SmartDashboard.putNumber("joystick  Y value", self.driverJoystick.getY())
+        #get x axis - 0 (when moving joystick sideways)
+        wpilib.SmartDashboard.putNumber("joystick  X value", self.driverJoystick.getX())
+        #get the rotational value - 2 (when twisting joystick) left: -1, right: 1
+        wpilib.SmartDashboard.putNumber("joystick  Z value", self.driverJoystick.getZ())
+
+        # wpilib.SmartDashboard.putNumber("joystick  radians value", self.driverJoystick.getDirectionRadians())
+        # wpilib.SmartDashboard.putNumber("joystick  throttle", self.driverJoystick.getThrottle())
+        # wpilib.SmartDashboard.putNumber("joystick  get top", self.driverJoystick.getTop())
+        # wpilib.SmartDashboard.putNumber("joystick  trigger", self.driverJoystick.getTrigger())
+        # wpilib.SmartDashboard.putNumber("joystick  get twist", self.driverJoystick.getTwist())
+
+        # self.driveTrain.tank_drive(self.speed*leftJoystick, self.speed*rightJoystick)
+        
 
 
 if __name__ == '__main__':
