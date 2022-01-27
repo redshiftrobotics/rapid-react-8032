cfea976e13d681620fd0e6902162a30581ba5f11
Jenna-LI <58526103+Jenna-LI@users.noreply.github.com>
2022-01-26 17:09:16 -0800

    added auto

---
+++ robot/autonomous/drive_forward.py
@@ -0,0 +1,15 @@
+from magicbot import AutonomousStateMachine, timed_state, state
+import wpilib
+
+from components.driveTrain import DriveTrain
+
+class drive_forward(AutonomousStateMachine):
+
+    MODE_NAME = "Drive Forward"
+    DEFAULT = True
+
+    driveTrain: DriveTrain
+
+    @timed_state(duration=3, first = True)
+    def drive_forward(self):
+        self.driveTrain.arcade_drive(0, 0.1) 
