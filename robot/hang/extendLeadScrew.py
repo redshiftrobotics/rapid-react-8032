76854cddc3c1886ebf3add9ca39c615c3a3bd372
Jenna-LI <58526103+Jenna-LI@users.noreply.github.com>
2022-02-07 17:40:40 -0800

    properly formatted

--- robot/hang/extendLeadScrew.py
+++ robot/hang/extendLeadScrew.py
@@ -2,18 +2,16 @@ from magicbot import StateMachine, state
 import wpilib
 from components.hangComponents import HangComponents
 
+
 class ExtendLeadScrew(StateMachine):
-    hangComponents:HangComponents
+    hangComponents: HangComponents
 
     def extendLeadScrew(self):
-        self.engage() # type:ignore
-    
+        self.engage()  # type:ignore
+
     @state(first=True)
     def startExtendLeadScrew(self):
-        
-        #the motor speed defaults back to zero unless otherwise called.
+
+        # the motor speed defaults back to zero unless otherwise called.
         if not self.hangComponents.getTopLeadScrewSensor():
             self.hangComponents.setLeadScrewMotorSpeed(1.0)
-
-        
-
