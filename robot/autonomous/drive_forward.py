83a060b4c9706d51683124dc0cbf5e0efb7e6e7f
Jenna-LI <58526103+Jenna-LI@users.noreply.github.com>
2022-02-07 17:38:33 -0800

    added extend lead screw

--- robot/autonomous/drive_forward.py
+++ robot/autonomous/drive_forward.py
@@ -2,14 +2,13 @@ from magicbot import AutonomousStateMachine, timed_state, default_state
 import wpilib
 from components.driveTrain import DriveTrain
 
-
 class DriveForward(AutonomousStateMachine):
 
     MODE_NAME = "Drive Forward"
     DEFAULT = False
 
-    driveTrain: DriveTrain
+    driveTrain: DriveTrain    
 
-    @timed_state(duration=3, first=True)
+    @timed_state(duration=3, first = True)
     def driveForward(self):
-        self.driveTrain.arcadeDrive(0, 0.1)
+        self.driveTrain.arcadeDrive(0, 0.1) 
---
+++ robot/hang/extendLeadScrew.py
@@ -0,0 +1,19 @@
+from magicbot import StateMachine, state
+import wpilib
+from components.hangComponents import HangComponents
+
+class ExtendLeadScrew(StateMachine):
+    hangComponents:HangComponents
+
+    def extendLeadScrew(self):
+        self.engage() # type:ignore
+    
+    @state(first=True)
+    def startExtendLeadScrew(self):
+        
+        #the motor speed defaults back to zero unless otherwise called.
+        if not self.hangComponents.getTopLeadScrewSensor():
+            self.hangComponents.setLeadScrewMotorSpeed(1.0)
+
+        
+
