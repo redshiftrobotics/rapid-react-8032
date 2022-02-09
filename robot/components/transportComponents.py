ce2db4007b1e2fd57518c29e8e984cb25173de8c
AchillesHam <achillesham@seattleacademy.org>
2022-02-09 15:59:14 -0800

    Added enable and disable functions

--- robot/components/transportComponents.py
+++ robot/components/transportComponents.py
@@ -1,24 +1,23 @@
 import wpilib
 import rev
 
-
 class Transport:
     intakeMotor: rev.CANSparkMax
 
     def __init__(self):
         self.enabled = False
 
-        self.intakeMotorSpeed = 0
-        self.hangPulleyMotorSpeed = 0
-        self.dropperMotorSpeed = 0
-        self.leadScrewMotorSpeed = 0
-
-    def intake(self, intakeSpeed: float):
+    def setIntakeSpeed(self, intakeSpeed: float):
         self.intakeMotorSpeed = intakeSpeed
 
-    def execute(self):
-        wpilib.SmartDashboard.putNumber("intake motor", self.intakeMotorSpeed)
+    def enable(self):
+        self.enabled = True
+    
+    def disable(self):
+        self.enabled = False
 
+    def execute(self):
+        #wpilib.SmartDashboard.putNumber("intake motor", self.intakeMotorSpeed)
         if self.enabled:
             self.intakeMotor.set(self.intakeMotorSpeed)
 
