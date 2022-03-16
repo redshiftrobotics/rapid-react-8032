0d121d0e5c61f5697c41fd1c629e04a6a9a53529
kavidey <kaviasher@gmail.com>
2022-03-16 09:59:44 -0700

    Clean up float array generation

--- robot/utils/test_accel_limiter.py
+++ robot/utils/test_accel_limiter.py
@@ -3,7 +3,7 @@ from util import AccelerationLimiter
 
 limiter = AccelerationLimiter(10000)
 
-joystick_values = [0.0]*10 + [-0.05, -0.10, -0.15, -0.20, -0.25, -0.30, -0.35, -0.40, -0.45, -0.50, -0.55, -0.60, -0.65, -0.70, -0.75, -0.80, -0.85, -0.90, -0.95] + [-1.0]*10
+joystick_values = [0.0]*10 + [x / 20 for x in range(-1, -20, -1)] + [-1.0]*10
 
 for value in joystick_values:
     limiter.calculate(value)
